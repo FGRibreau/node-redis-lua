a0be30c33367ee9c3e20ad55157236380eb9eeb6
FG Ribreau <github@fgribreau.com>
2012-09-27 21:41:30 +0200

    Clean as node_redis supports eval

--- redis-lua.js
+++ redis-lua.js
@@ -2,23 +2,6 @@ var createHash = require('crypto').createHash,
     _          = require('lodash'),
     sha        = function(str) {return createHash('sha1').update(str).digest('hex');};
 
-/**
- * [eval_cmd description]
- * @param  {[type]}   db     [description]
- * @param  {[type]}   script [description]
- * @param  {[type]}   params [description]
- * @param  {Function} cb     [description]
- * @return {[type]}          [description]
- */
-function eval_cmd(db, script, params, cb) {
-  params.unshift(script);
-  db.send_command('eval', params, cb);
-}
-
-function evalsha_cmd(db, script_sha, params, cb) {
-  params.unshift(script_sha);
-  db.send_command('evalsha', params, cb);
-}
 
 function keyval(cb) {
   return function(err, res) {
@@ -71,21 +54,9 @@ exports.attachLua = function(redis) {
         cb = keyval(cb);
       }
 
-      if (!script_sha) {
-        script_sha = script_sha || sha(script);
-        eval_cmd(self, script, params, cb);
-        return;
-      }
-
-      evalsha_cmd(self, script_sha, params, function(err, res) {
-        if (err && err.message.indexOf('NOSCRIPT') > 0) {
-          // @todo test this part
-          eval_cmd(self, script, params.slice(2, -1), cb);
-        } else {
-          cb(err, res);
-        }
-      });
-
+      // node_redis now support eval
+      // https://github.com/mranney/node_redis/blob/master/index.js#L1067
+      this.eval(script, params, cb);
     };
 
     return this;
